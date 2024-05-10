e36a1da06e6f783a575be0c26ba3d56915b3f7cd
Gowrisankar S <114475480+gowrisankarsg@users.noreply.github.com>
2024-05-10 22:10:20 +0530

    Update urls.py

--- song/urls.py
+++ song/urls.py
@@ -20,7 +20,7 @@ from songApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('upload/', views.upload, name='upload'),
     path('link/', views.link, name='link')
 ]
